f243ad0c071ecf9846d1435e446a43724403c872
Tantely Randriamazaoro <tantely@mazaoro.com>
2025-01-07 22:44:00 +0300

    feat: Add schema for jobs and search tables

---
+++ sql/schema.sql
@@ -0,0 +1,23 @@
+CREATE TABLE search (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  query TEXT NOT NULL,
+  keywords TEXT,
+  timestamp DATETIME DEFAULT CURRENT_TIMESTAMP,
+  page_number INTEGER NOT NULL DEFAULT 1, -- Tracks the page of the search
+  board TEXT
+);
+
+CREATE TABLE jobs (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  search_id INTEGER NOT NULL REFERENCES search(id),
+  title TEXT NOT NULL,
+  link TEXT NOT NULL,
+  company TEXT,
+  location TEXT,
+  description TEXT,
+  salary TEXT,
+  timestamp DATETIME DEFAULT CURRENT_TIMESTAMP,
+  board TEXT
+  custom_fields TEXT
+  status TEXT NOT NULL DEFAULT 'Discovered'
+);
