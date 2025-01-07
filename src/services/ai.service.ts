c9b44f7cce1bdee353f9150652a2fd4183119f48
Tantely Randriamazaoro <tantely@mazaoro.com>
2025-01-07 22:43:45 +0300

    feat: added openai npm package for ai service

--- src/services/ai.service.ts
+++ src/services/ai.service.ts
@@ -1,12 +1,15 @@
 import { inject, injectable } from "inversify";
 import DatabaseService from "./database.service";
+import OpenAI from "openai";
 
 @injectable()
 export default class AiService {
+    private openai: OpenAI;
+
     constructor(
         @inject(DatabaseService) private databaseService: DatabaseService,
         options: { apiKey: string; }
     ) {
-
+        this.openai = new OpenAI({ apiKey: options.apiKey });
     }
 }
