482a6dd97c45e7691784efa720fdef923c286fcf
Tantely Randriamazaoro <tantely@mazaoro.com>
2025-01-16 01:14:32 +0300

    refactor: Update browser service to support headless mode and custom Chrome instance

--- src/services/core/browser.service.ts
+++ src/services/core/browser.service.ts
@@ -2,11 +2,14 @@ import Job, { JobDetails, LeverCustomFieldCard, NormalizedCustomField, RawCustom
 import chalk from "chalk";
 import { inject, injectable } from "inversify";
 import ora, { Ora } from "ora";
-import puppeteer, { Browser, Page, Puppeteer } from "puppeteer";
+import puppeteer, { Browser, Cookie, Page, Puppeteer } from "puppeteer";
 import AiService from "./ai.service";
 import LeverService from "../boards/lever.service";
 import Boards from "@/models/boards";
 import { EventEmitter } from "events";
+import InquirerService from "./inquirer.service";
+import { exec } from "child_process";
+import axios from "axios";
 
 @injectable()
 export default class BrowserService {
@@ -16,10 +19,85 @@ export default class BrowserService {
     constructor(
         @inject(AiService) private aiService: AiService,
         @inject(LeverService) private leverService: LeverService,
+        @inject(InquirerService) private inquirerService: InquirerService
     ) { }
 
     public async init(options: { headless: boolean }): Promise<void> {
-        this.browser = await puppeteer.launch({ headless: options.headless });
+        if (options.headless) {
+            this.browser = await puppeteer.launch({
+                headless: true
+            });
+        } else {
+            // Using a custom Chrome instance to resolve hCaptcha cookie issues and keep the browser open.
+
+            // Check current OS
+            const os = process.platform;
+            let command = '';
+
+            if (os === 'darwin') {
+                command = `/Applications/Google\\ Chrome.app/Contents/MacOS/Google\\ Chrome --remote-debugging-port=9222 --no-first-run --no-default-browser-check --user-data-dir=$(mktemp -d -t 'chrome-remote_data_dir')`;
+            } else if (os === 'win32') {
+                command = `start chrome --remote-debugging-port=9222 --no-first-run --no-default-browser-check --user-data-dir=$(mktemp -d -t 'chrome-remote_data_dir')`;
+            } else {
+                console.error(chalk.red('Unsupported OS:', os));
+                return;
+            }
+
+            try {
+                // Start Chrome process
+                this.runCommand(command);
+
+                // Wait for a short duration to allow Chrome to initialize
+                await new Promise((resolve) => setTimeout(resolve, 3000));
+
+                // Fetch the browser ID from the debugging endpoint
+                const browserId = await this.getBrowserIdFromEndpoint();
+                console.log(chalk.green(`Browser ID: ${browserId}`));
+
+                this.browser = await puppeteer.connect({
+                    browserWSEndpoint: 'ws://127.0.0.1:9222/devtools/browser/' + browserId
+                });
+            } catch (error) {
+                console.error(chalk.red('Failed to initialize the browser:'), error);
+            }
+        }
+    }
+
+    private runCommand(command: string) {
+        const child = exec(command, (error, stdout, stderr) => {
+            if (error) {
+                console.error(chalk.red(error.message));
+            }
+            if (stderr) {
+                console.error(chalk.red(stderr));
+            }
+        });
+
+        child.stdout?.on('data', (data) => {
+            console.log(chalk.blue(data.toString()));
+        });
+    }
+
+    private async getBrowserIdFromEndpoint(): Promise<string> {
+        try {
+            const response = await axios.get('http://127.0.0.1:9222/json/version');
+            const wsEndpoint = response.data.webSocketDebuggerUrl;
+            const match = wsEndpoint.match(/devtools\/browser\/([\w-]+)/);
+
+            if (match && match[1]) {
+                return match[1];
+            } else {
+                throw new Error('Browser ID not found in WebSocket URL.');
+            }
+        } catch (error: any) {
+            throw new Error(`Failed to fetch browser ID from endpoint: ${error.message}`);
+        }
+    }
+
+
+
+    public async setCookie(cookie: Cookie) {
+        await this.browser?.setCookie(cookie);
     }
 
     public async closeBrowser(): Promise<void> {
