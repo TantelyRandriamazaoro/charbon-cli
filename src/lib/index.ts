093cd6b47648e6086720738e3549affc6991379f
Tantely Randriamazaoro <tantely@mazaoro.com>
2025-01-21 10:14:27 +0300

    feat: added boilerplate for programmatic use of @charbon/core

---
+++ src/lib/index.ts
@@ -0,0 +1,40 @@
+export class Charbon {
+    constructor() {
+    }
+
+    search() {
+        console.log('search');
+    }
+
+    scrape() {
+
+    }
+
+    bulkScrape() {
+
+    }
+
+    review() {
+
+    }
+
+    bulkReview() {
+
+    }
+
+    prepare() {
+
+    }
+
+    bulkPrepare() {
+
+    }
+
+    apply() {
+
+    }
+
+    bulkApply() {
+
+    }
+}
